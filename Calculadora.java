0ba9f3f29f3e5e14836e98cdb13eee3dd8ff7461
GabrielLacerda00 <gabriel.lacerd.gr@gmail.com>
2023-06-07 00:18:44 -0300

    refactoring: Rename Methods

--- Calculadora.java
+++ Calculadora.java
@@ -3,13 +3,13 @@ public class Calculadora {
     int resultado = 0;
 
     if (operacao.equals("soma")) {
-      resultado = soma(num1, num2);
+      resultado = sum(num1, num2);
     } else if (operacao.equals("subtracao")) {
-      resultado = subtracao(num1, num2);
+      resultado = minus(num1, num2);
     } else if (operacao.equals("multiplicacao")) {
-      resultado = multiplicacao(num1, num2);
+      resultado = mult(num1, num2);
     } else if (operacao.equals("divisao")) {
-      resultado = divisao(num1, num2);
+      resultado = div(num1, num2);
     } else {
       throw new IllegalArgumentException("Operação desconhecida: " + operacao);
     }
@@ -17,19 +17,19 @@ public class Calculadora {
     return resultado;
   }
 
-  private int soma(int num1, int num2) {
+  private int sum(int num1, int num2) {
     return num1 + num2;
   }
 
-  private int subtracao(int num1, int num2) {
+  private int minus(int num1, int num2) {
     return num1 - num2;
   }
 
-  private int multiplicacao(int num1, int num2) {
+  private int mult(int num1, int num2) {
     return num1 * num2;
   }
 
-  private int divisao(int num1, int num2) {
+  private int div(int num1, int num2) {
     if (num2 != 0) {
       return num1 / num2;
     } else {
