e51a050ae5ec785c78bdb7c9f77df06b29f699fe
GabrielLacerda00 <gabriel.lacerd.gr@gmail.com>
2023-07-03 09:08:12 -0300

    Rename Method: testing error in callsMethods

--- Calculadora.java
+++ Calculadora.java
@@ -6,11 +6,11 @@ public class Calculadora {
       resultado = sum(num1, num2);
       resultado = soma(num1, num2);
     } else if (operacao.equals("subtracao")) {
-      resultado = minus(num1, num2);
+      resultado = subtracao(num1, num2);
     } else if (operacao.equals("multiplicacao")) {
-      resultado = mult(num1, num2);
+      resultado = multiplicacao(num1, num2);
     } else if (operacao.equals("divisao")) {
-      resultado = div(num1, num2);
+      resultado = divisao(num1, num2);
     } else {
       throw new IllegalArgumentException("Operação desconhecida: " + operacao);
     }
@@ -22,15 +22,15 @@ public class Calculadora {
     return num1 + num2;
   }
 
-  private int minus(int num1, int num2) {
+  private int subtracao(int num1, int num2) {
     return num1 - num2;
   }
 
-  private int mult(int num1, int num2) {
+  private int multiplicacao(int num1, int num2) {
     return num1 * num2;
   }
 
-  private int div(int num1, int num2) {
+  private int divisao(int num1, int num2) {
     if (num2 != 0) {
       return num1 / num2;
     } else {
